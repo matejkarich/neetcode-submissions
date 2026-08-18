e410d3ca958c81fc575b44f726fdd0942a52e208
Richard Matejka <62668489+matejkarich@users.noreply.github.com>
2026-08-17 20:57:40 -0700

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.py
@@ -0,0 +1,62 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+        self.serializedTree = []
+        self.queue = []
+
+        return "".join(self.bfs(root)).rstrip("_")
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if data == "":
+            return None
+        rootNode = TreeNode(data[0], None, None)
+        self.queue = [rootNode]
+        self.deserializeBfs(data, 1)
+        return rootNode
+
+    def deserializeBfs(self, data, index):
+        if not self.queue or index >= len(data):
+            return
+        parent = self.queue.pop(0)
+        if data[index] == '_':
+            leftChild = None
+        else:
+            leftChild = TreeNode(data[index], None, None)
+            self.queue.append(leftChild)
+
+        if data[index+1] == '_':
+            rightChild = None
+        else:
+            rightChild = TreeNode(data[index+1], None, None)
+            self.queue.append(rightChild)
+
+        parent.left = leftChild
+        parent.right = rightChild
+        self.deserializeBfs(data, index+2)
+
+
+    def bfs(self, node):
+        print(self.queue)
+        if not node:
+            self.serializedTree.append("_")
+        else:        
+            self.queue.append(node.left)
+            self.queue.append(node.right)
+            self.serializedTree.append(str(node.val))
+
+        if not self.queue:
+            print(self.serializedTree)
+            return self.serializedTree
+        return self.bfs(self.queue.pop(0))
