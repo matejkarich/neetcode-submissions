f56342d023a0c26a30ae20a77c5ad4daad08b7b2
Richard Matejka <62668489+matejkarich@users.noreply.github.com>
2026-08-28 21:46:29 -0700

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,55 @@
+class TrieNode():
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEndOfWord = False
+        self.wordsInPath = 0
+
+    def addWord(self, word):
+        curr = self
+        for c in word:
+            index = ord(c) - ord('a')
+            if not curr.children[index]:
+                newNode = TrieNode()
+                curr.children[index] = newNode
+            curr.wordsInPath += 1
+            curr = curr.children[index]
+        curr.isEndOfWord = True
+        curr.wordsInPath += 1
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        ROWS, COLS = len(board), len(board[0])
+        root = TrieNode()
+        result = []
+
+        for w in words:
+            root.addWord(w)
+
+        def dfs(r, c, node, word):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS:
+                return
+            index = ord(board[r][c])-ord('a')
+            if board[r][c] == '*' or not node.children[index] or node.children[index].wordsInPath <= 0:
+                return
+
+            letter = board[r][c]
+            word += letter
+            board[r][c] = '*'
+            node = node.children[index]
+            if node.isEndOfWord:
+                result.append(word)
+                node.wordsInPath -= 1
+
+            dfs(r - 1, c, node, word)
+            dfs(r + 1, c, node, word)
+            dfs(r, c - 1, node, word)
+            dfs(r, c + 1, node, word)
+
+            board[r][c] = letter 
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+
+        return result
+        
