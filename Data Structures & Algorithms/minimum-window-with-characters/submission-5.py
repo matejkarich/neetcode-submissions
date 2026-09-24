52e95b0fa35b42dfefc5f91e51bd2706ab334a03
Richard Matejka <62668489+matejkarich@users.noreply.github.com>
2026-09-23 19:28:06 -0700

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        windowMap = {}
+        tMap = {}
+        for char in t:
+            if char in tMap:
+                tMap[char] += 1
+            else:
+                tMap[char] = 1
+            windowMap[char] = 0
+
+        if len(s) < len(t):
+            return ""
+
+        start = 0
+        while start < len(s) and s[start] not in tMap:
+            start += 1
+
+        matchesInWindow = 0
+        result = (-1, -1)
+        end = start
+        while end < len(s):
+            if s[end] in windowMap:
+                windowMap[s[end]] += 1
+                if windowMap[s[end]] == tMap[s[end]]:
+                    matchesInWindow += 1
+                while matchesInWindow == len(tMap.keys()):
+                    if result == (-1,-1) or (end - start + 1) < (result[1] - result[0]):
+                        result = (start, end+1)
+                    if s[start] in windowMap:
+                        windowMap[s[start]] -= 1
+                        if windowMap[s[start]] < tMap[s[start]]:
+                            matchesInWindow -= 1
+                    start += 1
+            end += 1
+
+        return s[result[0]:result[1]]
+
+
+        
